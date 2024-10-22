e4bdee5db15a1ce6f305388c17aa44ac0d1525f8
VEERAMANY88 <veeramany88@gmail.com>
2024-10-22 19:19:33 +0530

    ui

--- daydashboard_api.py
+++ daydashboard_api.py
@@ -11,14 +11,14 @@ def get_data():
         "message": "Success",
         "day_level": [
             {
-                "trans_date": "2024-10-22",
-                "target": 16.0,
-                "achieved": 21.0,
-                "achieved_perc": 131.25,
+                "trans_date": "2024-10-24",
+                "target": 16,
+                "achieved": 21,
+                "achieved_perc": 131,
                 "color_code": 3.0
             },
             {
-            "trans_date": "2024-10-24",
+            "trans_date": "2024-10-23",
             "target": 180,
             "achieved": 9,
             "achieved_perc": 1,
@@ -35,7 +35,7 @@ def get_data():
                 "trans_date": "2024-10-21",
                 "target": 18.0,
                 "achieved": 23.0,
-                "achieved_perc": 127.78,
+                "achieved_perc": 127,
                 "color_code": 3.0
             }
         ]
