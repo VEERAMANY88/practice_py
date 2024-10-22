b257b557236a703bad51d018ea2d89754aee800d
VEERAMANY88 <veeramany88@gmail.com>
2024-10-22 19:05:59 +0530

    collor code

--- daydashboard_api.py
+++ daydashboard_api.py
@@ -18,6 +18,20 @@ def get_data():
                 "color_code": 3.0
             },
             {
+            "trans_date": "2024-10-24",
+            "target": 180,
+            "achieved": 9,
+            "achieved_perc": 1,
+            "color_code": 5.0
+            },
+            {
+            "trans_date": "2024-10-22",
+            "target": 16,
+            "achieved": 21,
+            "achieved_perc": 131,
+            "color_code": 2.0
+             },
+            {
                 "trans_date": "2024-10-21",
                 "target": 18.0,
                 "achieved": 23.0,
