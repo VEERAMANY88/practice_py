ca5057e75679eb25f3bb7f816604801505389443
VEERAMANY88 <veeramany88@gmail.com>
2024-12-03 06:31:46 +0530

    api to kpi

--- daydashboard_api.py
+++ daydashboard_api.py
@@ -4,7 +4,7 @@ from flask_cors import CORS  # Import CORS
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-@app.route('/api/data', methods=['GET'])
+@app.route('/kpi', methods=['GET'])
 def get_data():
     data = {
         "messagecode": 200,
